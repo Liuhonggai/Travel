823cd84f288e20e7ae065b5c42485fc33b798250
Tests <513786218@qq.com>
2015-05-22 10:47:51 +0800

    修改APP问题

---
+++ HappyWeekend/HappyWeekend/Detail/Model/Detail.h
@@ -0,0 +1,13 @@
+//
+//  Detail.h
+//  HappyWeekend
+//
+//  Created by lanou3g on 15/5/22.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Detail : NSObject
+
+@end
---
+++ HappyWeekend/HappyWeekend/Detail/Model/Detail.m
@@ -0,0 +1,13 @@
+//
+//  Detail.m
+//  HappyWeekend
+//
+//  Created by lanou3g on 15/5/22.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import "Detail.h"
+
+@implementation Detail
+
+@end
