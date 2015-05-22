a67b0d448aa84696fd5f83d24eb1995dc332e0c8
Tests <513786218@qq.com>
2015-05-22 10:12:49 +0800

    第二次提交

---
+++ HappyWeekend/HappyWeekend/Home/Model/Home.h
@@ -0,0 +1,13 @@
+//
+//  Home.h
+//  HappyWeekend
+//
+//  Created by lanou3g on 15/5/22.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Home : NSObject
+
+@end
---
+++ HappyWeekend/HappyWeekend/Home/Model/Home.m
@@ -0,0 +1,13 @@
+//
+//  Home.m
+//  HappyWeekend
+//
+//  Created by lanou3g on 15/5/22.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import "Home.h"
+
+@implementation Home
+
+@end
